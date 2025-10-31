b28b98c69bb88a37f17952515a19344bf584a8d1
Pedro Gallardo <pedrogallardo7011@gmail.com>
2025-10-31 16:08:36 -0300

    feat: add a new service for republica virtual api

---
+++ app/src/Service/RvPostalCodeService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Service;
+
+use Cake\Http\Client;
+
+
+/**
+ * Service to handle postal code information using Republica Virtual API.
+ */
+class RvPostalCodeService implements PostalCodeServiceInterface
+{
+    private Client $httpClient;
+    private string $apiUrl = 'http://cep.republicavirtual.com.br/web_cep.php';
+    
+    public function fetchPostalCode(string $postalCode): ?array
+    {
+        
+    }
+}
