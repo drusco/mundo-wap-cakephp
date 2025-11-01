853d0d43492e72a7f1f7ab58d528bea0b8e304ca
Pedro Gallardo <pedrogallardo7011@gmail.com>
2025-10-31 22:49:36 -0300

    feat: implement the postal code service logic

--- app/src/Service/PostalCodeService.php
+++ app/src/Service/PostalCodeService.php
@@ -4,12 +4,26 @@ namespace App\Service;
 use App\Service\Interface\PostalCodeServiceInterface;
 
 /**
- * Service to handle postal code using fallback logic.
+ * Service to handle postal code using primary and fallback services.
  */
 class PostalCodeService implements PostalCodeServiceInterface
 {
+    public function __construct(
+        private PostalCodeServiceInterface $primaryService,
+        private PostalCodeServiceInterface $fallbackService,
+    ){}
+
     public function fetchPostalCode(string $postalCode): ?array
     {
-        return null;
+        // call the primary service first
+        $result = $this->primaryService->fetchPostalCode($postalCode);
+        
+        if ($result === null) {
+            // call the fallback service if primary fails
+            $result = $this->fallbackService->fetchPostalCode($postalCode);
+        }
+
+        // return the final result
+        return $result;
     }
 }
