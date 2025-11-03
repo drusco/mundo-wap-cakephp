fb4e8ec625c9fbe750c6463f90fbadfe16c0941d
Pedro Gallardo <pedrogallardo7011@gmail.com>
2025-11-03 07:19:47 -0300

    feat: create method to move all pending visits to the next available date

--- app/src/Controller/WorkdaysController.php
+++ app/src/Controller/WorkdaysController.php
@@ -67,13 +67,85 @@ class WorkdaysController extends AppController
             throw new BadRequestException('Date query parameter expects the format: dd-mm-yyyy');
         }
 
+        // move all pending visits to the next available dates
+        $success = $this->movePendingVisits($dateObject);
+
          // return response
         $this->set([
-            'success' => true,
-            'data' => []
+            'success' => $success,
+            'message' => $success ? 'Workday was successfully closed' : 'Workday could not be closed'
         ]);
+
+        $this->response->withStatus($success ? 200 : 500);
         
-        $this->viewBuilder()->setOption('serialize', ['success', 'data']);
+        $this->viewBuilder()->setOption('serialize', ['success', 'message']);
+    }
+
+    /**
+     * Move all pending visits to the next available date
+     * 
+     * @param \DateTime A date to find pending visits
+     * @return bool Returnns true all visits were moved, false otherwise
+     */
+    private function movePendingVisits(\DateTime $date): bool
+    {
+        $maxDuration = 60 * 8;
+        $visitsTable = $this->fetchTable('Visits');
+        $workdaysTable = $this->fetchTable('Workdays');
+        $movedVisitsCount = 0;
+
+        $pendingVisits = $visitsTable->find()
+            ->where([
+                'date' => $date,
+                'completed' => 0
+            ])
+            ->orderAsc('id')
+            ->all();
+
+        if (!$pendingVisits->count()) {
+            // no pending visits were found
+            return true;
+        }
+
+        foreach ($pendingVisits as $visit) {
+            $formsMinutes = $visit->forms * 15;
+            $productsMinutes = $visit->products * 5;
+            $visitDuration = $formsMinutes + $productsMinutes;
+
+            $nextDate = (clone $date)->modify('+1 day');
+
+            // loop through the workdays to check if visits fit
+            while (true) {
+                /** @var \App\Model\Entity\Workday */
+                $workday = $workdaysTable->findOrCreate([
+                    'date' => $nextDate
+                ]);
+
+                // get current duration
+                $workdayDuration = $workday->duration;
+
+                // check if there is space in the next workday
+                if ($workdayDuration + $visitDuration <= $maxDuration) {
+                    // move the visit to the next date
+                    $visit->set('date', $nextDate);
+                    if ($visitsTable->save($visit)) {
+                        $movedVisitsCount++;
+                    }
+                    break;
+                }
+
+                // try to move to the next date
+                $nextDate = (clone $nextDate)->modify('+1 day');
+            }
+        }
+
+        // check whether the workday is free of pending visits
+        if ($pendingVisits->count() !== $movedVisitsCount) {
+            // some visits could not be moved to a next date
+            return false;
+        }
+
+        return true;
     }
 
 }
